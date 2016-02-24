fbfac14e8be11e13110abaacbf18b79802c3b9ea
Christopher Kenrick <christopher.kenrick@gmail.com>
2016-02-24 17:12:24 -0500

    First update in Section 2

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyAV6iehI7YK69O7gTC8UkmDVaw4DPhvh3o';
 
-// Create new functional component
-// Functional components are used to take in information and spit out jsx
-// Functional components can contain class based components
+class App extends Component {
+	constructor(props) {
+		super(props);
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			// With ES6 you can have the key value pair equal the 
+			// same thing as this.setState({ videos: videos })
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
-
-// Take this component's generated HTML and put it in the DOM
 
 ReactDOM.render(<App />, document.querySelector('.container'));
