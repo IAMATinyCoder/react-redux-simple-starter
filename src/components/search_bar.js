6f4b16edc507a5dac2b446193ba1cc9645e447ac
Christopher Kenrick <christopher.kenrick@gmail.com>
2016-02-24 16:57:08 -0500

    Update after Section 1

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-	return <input />;
-};
+// Create a class based component
+// Class based components are used when 
+// a component needs to be aware of state
+
+class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
+	}
+
+	render() {
+		return (
+			<div>
+				<input 
+				value ={this.state.term}
+				  />
+			</div>
+		); 
+	}
+}
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAV6iehI7YK69O7gTC8UkmDVaw4DPhvh3o';
 
-// Create new component. Should produce HTML
+// Create new functional component
+// Functional components are used to take in information and spit out jsx
+// Functional components can contain class based components
 
 const App = () => {
 	return (
